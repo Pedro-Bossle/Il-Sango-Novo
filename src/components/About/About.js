ae9d35a639e4fb303879812cf39e22f68d6a142b
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-04-03 13:12:59 -0300

    Criado os Cards
    Falta o texto deles, quero ajeitar os cards do banner pra ficarem mais suaves a transição de volta , próximo passo é a exibição das sessões e do calendário

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import "./About.css"
+import Card from "../Card/Card.js"
+
+const About = () => {
+    return (
+        <div className="about-section">
+            <h2 className="about-title">Nossos Dirigentes Espirituais</h2>
+            <p className="about-desc">Conheça os guias espirituais que conduzem nossa casa com amor, sabedoria e dedicação</p>
+                <Card></Card>
+            <h3 className="about-mission-title">Nossa Missão</h3>
+            <p className="about-mission-text">Servir como ponte entre o mundo material e espiritual, oferecendo orientação, cura e desenvolvimento espiritual a todos que buscam luz em suas jornadas.<br></br> O Ilê Sàngó Aganjù e Oṣun Pandá é um espaço de acolhimento, respeito e transformação.</p>
+        </div>
+    )
+}
+
+export default About;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,30 @@
-import "./Card.css"
+import './Card.css'
 
 const Card = () => {
     return (
-        <div>
-            <h2> </h2>
+        <div className="card-section">
+
+            <div className="card tiago">
+                <div className="card-titulo-container">
+                    <img className="card-foto" src="./images/tiago.jpg" alt="Tiago de Xango Aganju" />
+                    <div>
+                        <h3 className="card-nome">Tiago de Sàngó Aganjù</h3>
+                        <h5 className="card-sub">X Anos de Experiência</h5>
+                    </div>
+                </div>
+                <p className="card-historia">Iniciado por XX na tradição X em X, feito Mestre de Quimbanda Malei pelo feitor X da casa X (Outras informações caso queiram)</p>
+            </div>
+
+            <div className="card rosangela">
+                <div className="card-titulo-container">
+                    <img className="card-foto" src="./images/rosangela.png" alt="Rosangela de Oxum Panda" />
+                    <div>
+                        <h3 className="card-nome">Rosangela de Oṣun Pandá</h3>
+                        <h5 className="card-sub">X Anos de Experiência</h5>
+                    </div>
+                </div>
+                <p className="card-historia">Iniciado por XX na tradição X em X, feito Mestre de Quimbanda Malei pelo feitor X da casa X (Outras informações caso queiram)</p>
+            </div>
         </div>
     )
 }
