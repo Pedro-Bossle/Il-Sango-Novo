fd3dcdaeb947cc58dae96bd940696f4083169a47
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-04-14 10:47:48 -0300

    Corrigido erro do vercel

--- src/components/Eventos/Eventos.js
+++ src/components/Eventos/Eventos.js
@@ -1,4 +1,4 @@
-﻿import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { supabase } from '../../lib/supabaseClient';
 import EventCard from './EventCard';
